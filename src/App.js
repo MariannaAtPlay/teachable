69819c66c0dd7f9f2ddaf6206c820b97c0e007f9
Marianna Shvartsapel <marianna.shvartsapel@gmail.com>
2019-05-22 08:34:27 -0400

    refactor: Separate SearchGems into its own component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import * as GemsAPI from './GemsAPI';
-import SearchResultItem from './SearchResultItem';
+import SearchGems from './SearchGems';
 import './App.css';
 
 class App extends Component {
 	state = {
-		query: '',
-		searchResults: [],
-		savedGems: [],
-		localStorageAvailable: true
+		savedGems: []
 	};
 
 	componentDidMount() {
@@ -54,86 +51,10 @@ class App extends Component {
 		});
 	};
 
-	handleInputChange = (e) => {
-		const currentQuery = e.target.value;
-
-		this.setState({
-			query: currentQuery
-		});
-
-		//in case currentQuery is empty -> reset searchResults
-		if (currentQuery === '') {
-			this.setState({
-				searchResults: []
-			});
-		} else {
-			GemsAPI.search(currentQuery)
-				.then((data) => {
-					const { query } = this.state;
-					//if there are no results, or by the time callback is executed,
-					//the search field has been cleared, reset searchResults
-					if ((data && !data.length) || query === '') {
-						this.setState({
-							searchResults: []
-						});
-					} else {
-						this.setState({
-							searchResults: data
-						});
-					}
-				})
-				.catch((error) => console.error(error));
-		}
-	};
-
 	render() {
-		const { query, searchResults, savedGems, localStorageAvailable } = this.state;
+		const { savedGems } = this.state;
 
-		return (
-			<main>
-				<label>
-					Search for Ruby Gems
-					<input
-						type="search"
-						value={query}
-						placeholder="Search Gems..."
-						onChange={this.handleInputChange}
-						aria-label="Search for Ruby Gems"
-					/>
-				</label>
-
-				{!localStorageAvailable ? (
-					<h4>
-						Note: LocalStorage is not available, your gems will not be saved after you
-						close this page
-					</h4>
-				) : null}
-
-				{searchResults.length > 0 ? (
-					<Fragment>
-						<h3>Search Results</h3>
-						<ul>
-							{searchResults.map((gemInfo) => {
-								const saved = savedGems.some((currentGem) => {
-									return currentGem.name === gemInfo.name;
-								});
-
-								return (
-									<SearchResultItem
-										gemInfo={gemInfo}
-										handleSaveChange={this.handleSaveChange}
-										saved={saved}
-										key={gemInfo.name}
-									/>
-								);
-							})}
-						</ul>
-					</Fragment>
-				) : (
-					<h3>No results found</h3>
-				)}
-			</main>
-		);
+		return <SearchGems savedGems={savedGems} handleSaveChange={this.handleSaveChange} />;
 	}
 }
 
---
+++ src/SearchGems.js
@@ -0,0 +1,96 @@
+import React, { Component, Fragment } from 'react';
+import * as GemsAPI from './GemsAPI';
+import SearchResultItem from './SearchResultItem';
+
+class SearchGems extends Component {
+	state = {
+		query: '',
+		searchResults: [],
+		localStorageAvailable: true
+	};
+
+	handleInputChange = (e) => {
+		const currentQuery = e.target.value;
+
+		this.setState({
+			query: currentQuery
+		});
+
+		//in case currentQuery is empty -> reset searchResults
+		if (currentQuery === '') {
+			this.setState({
+				searchResults: []
+			});
+		} else {
+			GemsAPI.search(currentQuery)
+				.then((data) => {
+					const { query } = this.state;
+					//if there are no results, or by the time callback is executed,
+					//the search field has been cleared, reset searchResults
+					if ((data && !data.length) || query === '') {
+						this.setState({
+							searchResults: []
+						});
+					} else {
+						this.setState({
+							searchResults: data
+						});
+					}
+				})
+				.catch((error) => console.error(error));
+		}
+	};
+
+	render() {
+		const { query, searchResults, localStorageAvailable } = this.state;
+		const { handleSaveChange, savedGems } = this.props;
+
+		return (
+			<main>
+				<label>
+					Search for Ruby Gems
+					<input
+						type="search"
+						value={query}
+						placeholder="Search Gems..."
+						onChange={this.handleInputChange}
+						aria-label="Search for Ruby Gems"
+					/>
+				</label>
+
+				{!localStorageAvailable ? (
+					<h4>
+						Note: LocalStorage is not available, your gems will not be saved after you
+						close this page
+					</h4>
+				) : null}
+
+				{searchResults.length > 0 ? (
+					<Fragment>
+						<h3>Search Results</h3>
+						<ul>
+							{searchResults.map((gemInfo) => {
+								const saved = savedGems.some((currentGem) => {
+									return currentGem.name === gemInfo.name;
+								});
+
+								return (
+									<SearchResultItem
+										gemInfo={gemInfo}
+										handleSaveChange={handleSaveChange}
+										saved={saved}
+										key={gemInfo.name}
+									/>
+								);
+							})}
+						</ul>
+					</Fragment>
+				) : (
+					<h3>No results found</h3>
+				)}
+			</main>
+		);
+	}
+}
+
+export default SearchGems;
